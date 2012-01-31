c222f0e4206379c4f914a307ada7217b2ca232fa
Manoj Mathai <manojpmathai@gmail.com>
2012-02-01 00:21:46 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twonversations::Application.config.secret_token = 'a8ef9285758a5957d8d88d0e32e828ac458c34514ad0232b6f783b0e259e37661511871a5fd8344ec0353fb626a3970357704cca719a3b6bcd3ca1de8b1b66ae'
